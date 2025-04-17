edb31a62cf1607632d18809a5f2c5a8ff9d4fb0a
Pbruun <patrickbj95@hotmail.com>
2025-04-17 17:48:29 +0200

    correct dist

--- dnd-discord-app/vite.config.ts
+++ dnd-discord-app/vite.config.ts
@@ -10,22 +10,9 @@ export default defineConfig({
     vue(),
     vueDevTools()
   ],
-  build:{
-    commonjsOptions:{
-      exclude:['zlib-sync']
-    },
-    lib: {
-      entry: 'src/main.js',
-      formats: ['es'],
-    },
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
-    extensions:['.js', '.json', '.ts', '.vue']
   },
-  server:{
-    allowedHosts:true
-  }
 })
